9d56c5a8f9105b2b76ad234951bfabbfe29037a7
miguelrey0415 <101655447+miguelrey0415@users.noreply.github.com>
2022-06-23 13:15:23 -0500

    Update main.js

--- main.js
+++ main.js
@@ -25,22 +25,22 @@ recognition.addEventListener('result', (e)=>{
     if (text.includes("te amo") || text.includes('te amo')) {
       p = document.createElement('p');
       p.classList.add('replay');
-      p.innerText = 'Te amo mucho más mi vida hermosa, eres todo para mi y eres lo mejor de mi vida';
+      p.innerText = 'Te amo mucho más mi vida hermosa, eres todo para mi y eres lo mejor de mi vida, eres la unica persona en mi vida eso tenlo simpre presente';
       texts.appendChild(p)
     }
     if (text.includes("estoy triste") || text.includes('estoy triste ')) {
       p = document.createElement('p');
       p.classList.add('replay');
-      p.innerText = 'Mi niña hermosa recuerda que te amo demasido y que siempre estoy para ti, recuerda también que eres una persona increible y la niña más hermosa. Te mando millones de abrazos de mucho apoyo y para que ya no estes triste, Te amo con todo el corazón';
+      p.innerText = 'Mi niña hermosa recuerda que te amo demasido y que siempre estoy para ti, recuerda también que eres una persona increible y la niña más hermosa. Te mando millones de abrazos de mucho apoyo y para que ya no estes triste, Te amo con todo el corazón, desearia esta contigo en este momento para abrazarte y decirte que te amo tanto y ser tu apoyo siempre, recuerda eso siempre estare para ti en tus peores y mejores momentos';
       texts.appendChild(p)
     }
     if (text.includes('Reproduce Nuestra canción')) {
       p = document.createElement('p');
       p.classList.add('replay');
-      p.innerText = 'Te amo mucho y tu mandas';
+      p.innerText = 'Recuerda que esta canción siempre dira lo mucho que te amo mi niña preciosa';
       texts.appendChild(p)
       console.log('opening youtube')
-      window.open('https://www.youtube.com/watch?v=ELsgRAGIgw0')
+      window.open('https://www.youtube.com/watch?v=Nblbf7FGruE')
     }
     p = document.createElement('p');
   }
